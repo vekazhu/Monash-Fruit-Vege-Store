b0c6c8b7576e75952f7e4b005830627e608b7aac
VEKA <wzhu0006@student.monash.edu>
2018-10-05 22:36:42 +1000

    10:36

--- Customer.java
+++ Customer.java
@@ -51,24 +51,6 @@ public class Customer extends User
     {
         this.status = status;
     }
-
-    public void nameScanner()
-    {
-        System.out.println("Please enter your first Name between 3-20 letters and only letters are allowed.");
-        Scanner sc = new Scanner(System.in);
-        String enteredToken = sc.next().trim();
-        while (Validator.isFirstNameValid(enteredToken) == false)
-        {
-            System.out.println("Please renter your first name.");
-            enteredToken = sc.next().trim();
-        }
-        if (Validator.isFirstNameValid(enteredToken) == true)
-        {
-            System.out.println("Your first name is " + enteredToken + ".");
-            String firstName = enteredToken;
-            sc.close();
-        }
-    }
     
     public void addProductToCart()
     {
