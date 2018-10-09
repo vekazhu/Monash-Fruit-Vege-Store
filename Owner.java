3c23d849bc567d6fd71ec454de3549332d4b8b7f
VEKA <wzhu0006@student.monash.edu>
2018-10-09 11:37:23 +1100

    owner

--- Owner.java
+++ Owner.java
@@ -42,18 +42,53 @@ public class Owner extends User
     
     public String shelfLifeScanner()
     {
-        System.out.println("Please enter the shelf life (in weeks) of the new product, only numbers are allowed.");
+        System.out.println("Please enter the shelf life (in weeks) of the new product, only integers are allowed.");
         Scanner sc = new Scanner (System.in);
         String enteredToken = sc.next().trim();
-        while (Validator.isFirstNameValid(enteredToken) == false)
+        while (Validator.isInt(enteredToken) == false)
         {
+            System.out.println("Please enter integers only.");
             enteredToken = sc.next().trim();
         }
-        System.out.println("The name of new product is " + enteredToken + ".");
-        String newProductName = enteredToken;
+        System.out.println("Product life is " + enteredToken + " weeks.");
+        String newProductShelfLife = enteredToken;
         sc.close();
-        return newProductName;
+        return newProductShelfLife;
     }
+    
+    public String priceScanner()
+    {
+        System.out.println("Please enter the price per KG of the new product, only numbers are allowed.");
+        Scanner sc = new Scanner (System.in);
+        String enteredToken = sc.next().trim();
+        while (Validator.isDouble(enteredToken) == false)
+        {
+            System.out.println("Please enter numbers only.");
+            enteredToken = sc.next().trim();
+        }
+        System.out.println("Price per KG is " + enteredToken + ".");
+        String newProductShelfLife = enteredToken;
+        sc.close();
+        return newProductShelfLife;
+    }
+    
+    public int discountScanner()
+    {
+        System.out.println("Please enter the discount (e.g. 20 means 20% off) of the new product, only integers are allowed.");
+        Scanner sc = new Scanner (System.in);
+        String enteredToken = sc.next().trim();
+        while (Validator.isInt(enteredToken) == false)
+        {
+            System.out.println("Please enter integers only.");
+            enteredToken = sc.next().trim();
+        }
+        System.out.println("Product discount is " + enteredToken + "% off.");
+        int newProductDiscount = Integer.parseInt(enteredToken);
+        sc.close();
+        return newProductDiscount;
+    }
+    
+    
     
     
         
